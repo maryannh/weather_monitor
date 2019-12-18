2acd093af6e07fc1602b1882450398dd0af2d8aa
Mary-Ann Horley <maryann.horley@gmail.com>
2019-12-18 15:22:30 +0000

    added example config

---
+++ config.sample.py
@@ -0,0 +1,9 @@
+# PYMONGO
+MONGO_URI = YOUR_MONGO_CONNECTION_URI
+
+# GEO
+LAT = YOUR_LATITUDE
+LONG = YOUR_LONGITUDE
+
+# FLOOD API
+FLOOD_API_URL = "https://environment.data.gov.uk/flood-monitoring/id/floods"
